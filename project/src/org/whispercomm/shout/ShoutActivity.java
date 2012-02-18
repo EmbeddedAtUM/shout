d3a09b168ba94c0f3a7b4d95fd1f91ee35d307ee
Gulshan Singh <gulshan@umich.edu>
2012-02-18 02:54:00 -0500

    initial project commit

---
+++ project/src/org/whispercomm/shout/ShoutActivity.java
@@ -0,0 +1,13 @@
+package org.whispercomm.shout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoutActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
