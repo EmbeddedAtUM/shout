95daaeb6a77cc9612b89678b20668b504bdfc104
Gulshan Singh <gulshan@umich.edu>
2012-03-02 22:16:57 -0500

    completed custom adapter, list view items now alternate in color. listview is now optimized using tags

--- project/shout/src/main/java/org/whispercomm/shout/Shout.java
+++ project/shout/src/main/java/org/whispercomm/shout/Shout.java
@@ -3,14 +3,54 @@ package org.whispercomm.shout;
 import org.joda.time.DateTime;
 
 public class Shout {
+
+	/**
+	 * Holds the newest shout id. Used for creating IDs for new shouts.
+	 */
+	static long idCount = 0;
+
+	/**
+	 * The time when this shout was received
+	 */
 	DateTime date;
+
+	/**
+	 * The message contained by the Shout
+	 */
 	String content;
 
-	public Shout(String content) {
+	/**
+	 * Unique ID of shout
+	 */
+	long id;
+
+	public Shout(String content, DateTime date) {
 		this.content = content;
+		this.date = date;
+		this.id = idCount++;
 	}
 
+	/**
+	 * Returns the message contained by this Shout
+	 * 
+	 * @return message contained by this Shout
+	 */
 	String getContent() {
 		return content;
+	}
+
+	long getID() {
+		return id;
+	}
+
+	/**
+	 * Returns {@link DateTime} object representing the time since the shout has
+	 * been received
+	 * 
+	 * @return {@link DateTime} object representing the time since the shout has
+	 *         been received
+	 */
+	DateTime getTimePassed() {
+		return new DateTime(DateTime.now().getMillis() - date.getMillis());
 	}
 }
--- project/shout/src/main/java/org/whispercomm/shout/ShoutActivity.java
+++ project/shout/src/main/java/org/whispercomm/shout/ShoutActivity.java
@@ -2,6 +2,8 @@ package org.whispercomm.shout;
 
 import java.util.ArrayList;
 
+import org.joda.time.DateTime;
+
 import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
@@ -23,8 +25,9 @@ public class ShoutActivity extends ListActivity {
 		setContentView(R.layout.main);
 
 		ArrayList<Shout> shouts = new ArrayList<Shout>();
-		Shout shout = new Shout("Test");
-		shouts.add(shout);
+		for (int i = 0; i < 15; i++) {
+			shouts.add(new Shout("Item " + i, DateTime.now()));
+		}
 
 		TimelineAdapter adapter = new TimelineAdapter(
 				this.getApplicationContext(), shouts);
@@ -33,6 +36,11 @@ public class ShoutActivity extends ListActivity {
 		Log.v(TAG, "Finished onCreate");
     }
     
+	static class ViewHolder {
+		TextView message;
+		TextView age;
+	}
+
 	private class TimelineAdapter extends ArrayAdapter<Shout> {
 
 		ArrayList<Shout> items;
@@ -43,28 +51,64 @@ public class ShoutActivity extends ListActivity {
 		}
 
 		@Override
-		public View getView(int position, View view, ViewGroup parent) {
-
-			Log.v(TAG, "Entered getView");
-			// Inflate the view
-			if (view == null) {
-				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				view = inflater.inflate(R.layout.row, parent, false);
-				Log.v(TAG, "View inflated");
-			}
+		public View getView(int position, View convertView, ViewGroup parent) {
 
 			// Get current shout
 			Shout shout = items.get(position);
 			Log.v(TAG, "Shout: " + shout);
 
-			// Set shout text
-			if (shout != null) {
-				TextView message = (TextView) view.findViewById(R.id.message);
+			View rowView = convertView;
+			Log.v(TAG, "Entered getView");
+			// Inflate the view
+			if (rowView == null) {
+				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				rowView = inflater.inflate(R.layout.row, parent, false);
 
-				message.setText(shout.getContent());
-				Log.v(TAG, "Textview text set");
+				ViewHolder viewHolder = new ViewHolder();
+				viewHolder.message = (TextView) rowView
+						.findViewById(R.id.message);
+				viewHolder.age = (TextView) rowView.findViewById(R.id.age);
+
+				rowView.setTag(viewHolder);
+
+				Log.v(TAG, "View inflated");
 			}
-			return view;
+
+			// Gets the view information
+			ViewHolder holder = (ViewHolder) rowView.getTag();
+
+			// Creates the message string
+			long timePassed;
+			String unit;
+			DateTime time = shout.getTimePassed();
+			if (time.isAfter(60 * 1000)) {
+				timePassed = shout.getTimePassed().getMinuteOfHour();
+				unit = "minute";
+			} else if (time.isAfter(60 * 60 * 1000)) {
+				timePassed = shout.getTimePassed().getHourOfDay();
+				unit = "hour";
+			} else if (time.isAfter(60 * 60 * 24 * 1000)) {
+				timePassed = shout.getTimePassed().getDayOfWeek();
+				unit = "day";
+			} else {
+				timePassed = shout.getTimePassed().getSecondOfMinute();
+				unit = "second";
+			}
+				
+			String ageText = String.format("Message received %d %s%s ago.",
+					timePassed, unit, timePassed == 1 ? "" : "s");
+
+			Log.v(TAG, ageText);
+			holder.age.setText(ageText);
+			holder.message.setText(shout.getContent());
+			Log.v(TAG, "Textview text set");
+
+			if (shout.id % 2 == 0)
+				rowView.setBackgroundColor(0xFFA8FFFF);
+			else
+				rowView.setBackgroundColor(0xFFFFFFFF);
+
+			return rowView;
 		}
 
 	}
