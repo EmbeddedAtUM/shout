64db436d1cb4d8ab609a7f00eff61e4c1ba4eb14
David Adrian <davadria@umich.edu>
2012-03-15 21:08:10 -0400

    Content Provider: Added some URI endpoints

--- project/shout/src/main/java/org/whispercomm/shout/provider/ShoutProvider.java
+++ project/shout/src/main/java/org/whispercomm/shout/provider/ShoutProvider.java
@@ -19,11 +19,27 @@ public class ShoutProvider extends ContentProvider {
 	private static final UriMatcher sURIMatcher = new UriMatcher(
 			UriMatcher.NO_MATCH);
 
+	private static final int SHOUTS = 0;
+	private static final int SHOUTS_ID = 1;
+	private static final int SHOUTS_USER_ID = 2;
+	private static final int SHOUTS_TAG_ID = 3;
+	private static final int TAGS = 4;
+	private static final int TAGS_ID = 5;
+	private static final int USERS = 6;
+	private static final int USERS_ID = 7;
+	
 	/**
 	 * Initializer block to set URIs in the UriMatcher
 	 */
 	static {
-
+		sURIMatcher.addURI(AUTHORITY, "shouts", SHOUTS);
+		sURIMatcher.addURI(AUTHORITY, "shouts/#", SHOUTS_ID);
+		sURIMatcher.addURI(AUTHORITY, "shouts/user/#", SHOUTS_USER_ID);
+		sURIMatcher.addURI(AUTHORITY, "shouts/tag/#", SHOUTS_TAG_ID);
+		sURIMatcher.addURI(AUTHORITY, "tags", TAGS);
+		sURIMatcher.addURI(AUTHORITY, "tags/#", TAGS_ID);
+		sURIMatcher.addURI(AUTHORITY, "users", USERS);
+		sURIMatcher.addURI(AUTHORITY, "users/#", USERS_ID);
 	}
 
 	private SQLiteDatabase mDB;
@@ -60,15 +76,14 @@ public class ShoutProvider extends ContentProvider {
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		// TODO Auto-generated method stub
+		mDB = mDBHelper.getWritableDatabase();
 		return 0;
 	}
 
 	@Override
 	public int update(Uri uri, ContentValues values, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
+		mDB = mDBHelper.getWritableDatabase();
 		return 0;
 	}
-
-
 }
