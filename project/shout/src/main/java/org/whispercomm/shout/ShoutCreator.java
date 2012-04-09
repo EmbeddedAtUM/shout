4da6c3859ed26304e537b2c0894157c18d37ba49
Yue Liu <liuyue@umich.edu>
2012-04-08 20:24:06 -0400

    Add SignatureUtility, which is in charge of the key pair of the user and signing/verifying signatures.

---
+++ project/shout/src/main/java/org/whispercomm/shout/ShoutCreator.java
@@ -0,0 +1,47 @@
+package org.whispercomm.shout;
+
+import java.io.UnsupportedEncodingException;
+
+import org.joda.time.DateTime;
+import org.whispercomm.shout.network.NetworkInterface;
+import org.whispercomm.shout.provider.BasicShout;
+import org.whispercomm.shout.provider.ShoutProviderContract;
+
+import android.app.Activity;
+import android.util.Log;
+
+public class ShoutCreator {
+
+	static final String TAG = "******ShoutCreator******";
+	
+	NetworkInterface networkIf;
+	SignatureUtility signUtility;
+	User user;
+	
+	public ShoutCreator(Activity callerActivity){
+		this.networkIf = new NetworkInterface(callerActivity);
+		this.signUtility = new SignatureUtility(callerActivity);
+		this.user = signUtility.getUser();
+	}
+	
+	public boolean createShout(DateTime timestamp, String content, Shout shoutOri){
+		
+		//generate a new shout with its signature
+		byte[] signature;
+		try {
+			signature = signUtility.genShoutSignature(timestamp, user, content, shoutOri);
+		} catch (UnsupportedEncodingException e) {
+			Log.e(TAG, e.getMessage());
+			return false;
+		}
+		Shout shout = new BasicShout(timestamp, user, content, shoutOri, signature);
+	
+		// insert the shout to database and get its shout_id back
+		long shoutId = ShoutProviderContract.storeShout(shout);
+		
+		//call networkUtility to send the new shout out
+		networkIf.send(shoutId);
+		return true;
+	}
+	
+}
