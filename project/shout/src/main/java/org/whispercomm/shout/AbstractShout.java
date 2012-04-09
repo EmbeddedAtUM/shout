7a53be705122311377d69ba4687a28c5856a9061
Yue Liu <liuyue@umich.edu>
2012-04-08 21:41:26 -0400

    Add AbstractShout, which provides the functionality to generate hash.

---
+++ project/shout/src/main/java/org/whispercomm/shout/AbstractShout.java
@@ -0,0 +1,27 @@
+package org.whispercomm.shout;
+
+import java.io.UnsupportedEncodingException;
+
+import android.util.Log;
+
+public abstract class AbstractShout implements Shout {
+
+	private byte[] hashCode;
+
+	@Override
+	public byte[] getHash() {
+		if (hashCode == null) {
+			try {
+				this.hashCode = SignatureUtility.genShoutHash(getTimestamp(),
+						getSender(), getContent(), getOriginalShout());
+			} catch (UnsupportedEncodingException e) {
+				Log.e("AbstractShout",
+						"Cannot generate this shout's hash becasue "
+								+ e.getMessage());
+				return null;
+			}
+		}
+		return hashCode;
+	}
+
+}
