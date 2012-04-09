b0d596eefb3369c0b5ffaf3f79caf29bc4857993
Yue Liu <liuyue@umich.edu>
2012-04-09 15:14:12 -0400

    Network: merge the class OneBroadcast into NaiveBroadcast.

--- project/shout/src/main/java/org/whispercomm/shout/network/NaiveBroadcast.java
+++ project/shout/src/main/java/org/whispercomm/shout/network/NaiveBroadcast.java
@@ -1,9 +1,15 @@
 package org.whispercomm.shout.network;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Timer;
+import java.util.TimerTask;
 
+import org.whispercomm.manes.client.maclib.ManesFrameTooLargeException;
 import org.whispercomm.manes.client.maclib.ManesInterface;
 import org.whispercomm.shout.provider.ShoutProviderContract;
+
+import android.util.Log;
 
 /**
  * Service in charge of sending and receiving shouts from the MANES network
@@ -14,26 +20,24 @@ import org.whispercomm.shout.provider.ShoutProviderContract;
 public class NaiveBroadcast implements NetworkProtocol {
 
 	public static final String TAG = "******NaiveBroadcast******";
+
 	/**
-	 * instance of sendScheduler
+	 * rebroadcast period in millisecond
 	 */
-	SendScheduler sendScheduler;
+	public static long PERIOD = 30 * 60 * 1000;
+	/**
+	 * total number of re-sends for each message
+	 */
+	public static int RESEND_NUM = 20;
+	/**
+	 * timer for scheduling periodic re-broadcast
+	 */
+	Timer sendScheduler;
 	ManesInterface manesIf;
-	
-	public NaiveBroadcast(ManesInterface manesIf){
+
+	public NaiveBroadcast(ManesInterface manesIf) {
 		this.manesIf = manesIf;
-		this.sendScheduler = new SendScheduler(manesIf);
-	}
-
-	/**
-	 * scheduler that pushes shouts out into MANES network
-	 */
-	class SendScheduler extends Timer {
-		ManesInterface manesIf;
-
-		public SendScheduler(ManesInterface manesIf) {
-			this.manesIf = manesIf;
-		}
+		this.sendScheduler = new Timer();
 	}
 
 	@Override
@@ -42,9 +46,36 @@ public class NaiveBroadcast implements NetworkProtocol {
 	}
 
 	@Override
-	public void handleOutgoingAppShout(long ShoutId) {
-		sendScheduler.schedule(new OneBroadcast(sendScheduler, ShoutId, 0),
-				OneBroadcast.PERIOD);
+	public void handleOutgoingAppShout(long shoutId) {
+		// get the byte[] representation of the outgoing shout.
+		NetworkShout shout = new NetworkShout(shoutId);
+		final byte[] shoutBytes;
+		try {
+			shoutBytes = shout.toNetworkBytes();
+		} catch (UnsupportedEncodingException e) {
+			Log.e(TAG, e.getMessage());
+			return;
+		} catch (ShoutChainTooLongException e) {
+			Log.e(TAG, e.getMessage());
+			return;
+		}
+		// schedule periodic re-broadcast up to RESEND_NUM times.
+		long delay = 0;
+		for (int i = 1; i <= RESEND_NUM; i++) {
+			delay += PERIOD;
+			sendScheduler.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					try {
+						manesIf.send(shoutBytes);
+					} catch (ManesFrameTooLargeException e) {
+						Log.e(TAG, e.getMessage());
+					} catch (IOException e) {
+						Log.e(TAG, e.getMessage());
+					}
+				}
+			}, delay);
+		}
 	}
 
 	@Override
