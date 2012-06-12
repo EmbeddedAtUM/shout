f00812193ab13c7cbe8ae3aa964ed339adf09a68
David Adrian <davadria@umich.edu>
2012-06-11 23:05:45 -0400

    Clean up naming of custom widgets

---
+++ project/shout/src/org/whispercomm/shout/customwidgets/ShoutPreferenceCategory.java
@@ -0,0 +1,36 @@
+package org.whispercomm.shout.customwidgets;
+
+import org.whispercomm.shout.R;
+
+import android.content.Context;
+import android.preference.PreferenceCategory;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShoutPreferenceCategory extends PreferenceCategory {
+
+	public ShoutPreferenceCategory(Context context) {
+		super(context);
+	}
+
+	public ShoutPreferenceCategory(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public ShoutPreferenceCategory(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected View onCreateView(ViewGroup parent) {
+		View newView = super.onCreateView(parent);
+
+		newView.setBackgroundResource(R.drawable.preferencecategoryshape);
+
+		return newView;
+
+	}
+
+}
