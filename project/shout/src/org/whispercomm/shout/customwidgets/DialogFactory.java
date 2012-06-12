f763988e582a6e4cfb66a8c82c6c5c770a0ad492
David Adrian <davadria@umich.edu>
2012-06-12 14:16:21 -0400

    Link username preference to SignatureUtility.
    Add warning about changing username.

---
+++ project/shout/src/org/whispercomm/shout/customwidgets/DialogFactory.java
@@ -0,0 +1,20 @@
+package org.whispercomm.shout.customwidgets;
+
+import org.whispercomm.shout.R;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class DialogFactory {
+
+	public static AlertDialog buildUsernameChangeDialog(Context context,
+			DialogInterface.OnClickListener positive) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setMessage(R.string.username_change_warning)
+				.setCancelable(false)
+				.setPositiveButton("OK! OK! Now let me walk into Mordor...", positive);
+		return builder.create();
+	}
+
+}
---
+++ project/shout/src/org/whispercomm/shout/customwidgets/ShoutUsernamePreference.java
@@ -0,0 +1,98 @@
+package org.whispercomm.shout.customwidgets;
+
+import org.whispercomm.shout.User;
+import org.whispercomm.shout.id.SignatureUtility;
+import org.whispercomm.shout.id.UserNameInvalidException;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.util.Log;
+
+public class ShoutUsernamePreference extends ShoutEditTextPreference {
+
+	protected static final String TAG = ShoutUsernamePreference.class
+			.getSimpleName();
+
+	public ShoutUsernamePreference(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+		this.setListeners();
+	}
+
+	public ShoutUsernamePreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		this.setListeners();
+	}
+
+	public ShoutUsernamePreference(Context context) {
+		super(context);
+		this.setListeners();
+	}
+
+	@Override
+	public void setOnPreferenceClickListener(
+			OnPreferenceClickListener onPreferenceClickListener) {
+		super.setOnPreferenceClickListener(onPreferenceClickListener);
+	}
+
+	private void setListeners() {
+		this.setOnPreferenceClickListener(preListener);
+		this.setOnPreferenceChangeListener(postListener);
+	}
+
+	private OnPreferenceClickListener preListener = new OnPreferenceClickListener() {
+
+		private OnClickListener positive = new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+
+		};
+
+		@Override
+		public boolean onPreferenceClick(Preference preference) {
+			Log.v(TAG, "Username preference clicked");
+
+			AlertDialog dialog = DialogFactory.buildUsernameChangeDialog(
+					getContext(), positive);
+			dialog.show();
+			return true;
+		}
+	};
+
+	private OnPreferenceChangeListener postListener = new OnPreferenceChangeListener() {
+
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			String newName = (String) newValue;
+			SignatureUtility signUtility = SignatureUtility.getInstance();
+			User current = signUtility.getUser(); // TODO Fix lifecycle
+			if (current == null) {
+				try {
+					signUtility.updateUserName(newName);
+				} catch (UserNameInvalidException e) {
+					Log.v(TAG, e.getMessage());
+					return false;
+				}
+				return true;
+			}
+			String oldName = current.getUsername();
+			if (!oldName.equals(newName)) {
+				try {
+					signUtility.updateUserName(newName);
+					Log.v(TAG, "Updated user name to: " + newName);
+					return true;
+				} catch (UserNameInvalidException e) {
+					Log.v(TAG, e.getMessage());
+				}
+			}
+			return false;
+		}
+	};
+}
