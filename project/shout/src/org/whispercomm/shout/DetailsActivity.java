2af9a35801a74cfb3407d09ce8a1d1ad4149953c
Anthony Tesija <atesija@umich.edu>
2012-06-20 14:50:02 -0400

    Work on DetailsActivity

--- project/shout/src/org/whispercomm/shout/DetailsActivity.java
+++ project/shout/src/org/whispercomm/shout/DetailsActivity.java
@@ -1,19 +1,26 @@
 
 package org.whispercomm.shout;
 
+import org.whispercomm.shout.ShoutActivity.ViewHolder;
 import org.whispercomm.shout.provider.ShoutProviderContract;
 
 import android.app.ListActivity;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends ListActivity {
 
+	private static final String TAG = "DetailsActivity";
 	public static final String SHOUT_ID = "shout_id";
+	private Shout shout;
 	
 	private Cursor cursor;
 	
@@ -22,6 +29,8 @@ public class DetailsActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.details);
+		//get shout id from intent
+		//shoutprovidercontract with id
 		int shoutId = 1; // FIXME
 		cursor = ShoutProviderContract.getCursorOverShoutComments(getApplicationContext(), shoutId);
 		setListAdapter(new CommentsAdapter(this, cursor));
@@ -31,6 +40,13 @@ public class DetailsActivity extends ListActivity {
 	public void onDestroy() {
 		super.onDestroy();
 		cursor.close();
+		Log.v(TAG, "Finished onDestroy");
+	}
+	
+	static class DetailsViewHolder {
+		TextView origSender;
+		TextView message;
+		TextView age;
 	}
 
 	private class CommentsAdapter extends CursorAdapter {
@@ -45,6 +61,7 @@ public class DetailsActivity extends ListActivity {
 			/*
 			 * Set the text in the relevant view fields.
 			 */
+			// Get the shout
 		}
 
 		@Override
@@ -53,7 +70,15 @@ public class DetailsActivity extends ListActivity {
 			// These view come from comment.xml
 			// You may want to make a class similar to ShoutActivity.ViewHolder
 			// to do this.
-			return null;
+			LayoutInflater inflater = LayoutInflater.from(context);
+			View rowView = inflater.inflate(R.layout.comment, parent, false);
+			DetailsViewHolder holder = new DetailsViewHolder();
+			holder.origSender = (TextView) rowView.findViewById(R.id.origsender);
+			holder.age = (TextView) rowView.findViewById(R.id.age);
+			holder.message = (TextView) rowView.findViewById(R.id.message);
+			rowView.setTag(holder);
+			Log.v(TAG, "View inflated");
+			return rowView;
 		}
 	}
 }
