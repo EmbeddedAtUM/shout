077e01f7d4df04572adec71b2031ab65aa2b93a2
David R. Bild <drbild@umich.edu>
2012-06-25 23:23:07 -0400

    add BroadcastReceiver to start background service on boot.

---
+++ project/shout/src/org/whispercomm/shout/network/BootReceiver.java
@@ -0,0 +1,40 @@
+package org.whispercomm.shout.network;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+/**
+ * Starts the Shout network service on system boot, if the user has enabled it.
+ * 
+ * @author David R. Bild
+ * 
+ */
+public class BootReceiver extends BroadcastReceiver {
+	private static final String TAG = BootReceiver.class.getSimpleName();
+
+	public static final String START_SERVICE_ON_BOOT = "runInBackground";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.i(TAG, "BOOT_COMPLETED received.");
+
+		SharedPreferences settings = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		boolean startOnBoot = settings.getBoolean(START_SERVICE_ON_BOOT, true);
+
+		if (startOnBoot) {
+			Log.i(TAG, "Starting Shout network service.");
+			Intent service = new Intent(context, NetworkService.class);
+			context.startService(service);
+		} else {
+			Log.i(TAG,
+					"Not starting Shout network service because user has disabled autostart.");
+		}
+
+	}
+
+}
