de2963b3fc2074a875a7810bde2cc801e3690290
David R. Bild <drbild@umich.edu>
2012-06-25 23:22:23 -0400

    add preference to control state of background service.

--- project/shout/src/org/whispercomm/shout/SettingsActivity.java
+++ project/shout/src/org/whispercomm/shout/SettingsActivity.java
@@ -1,9 +1,12 @@
 package org.whispercomm.shout;
 
+import org.whispercomm.shout.network.NetworkService;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
@@ -16,25 +19,34 @@ public class SettingsActivity extends PreferenceActivity {
 	boolean autoRotatePreference;
 	String usernamePreference;
 
+	private RunInBackgroundListener runInBackgroundListener;
+	private Preference runInBackgroundPref;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings);
 		addPreferencesFromResource(R.xml.preferences);
-		
+
 		// Setup Twitter Login preference click listener
 		Preference twitterLoginPref = (Preference) findPreference("twitterLoginPref");
 		twitterLoginPref
 				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
+					public boolean onPreferenceClick(Preference preference) {
 						Log.v(TAG, "twitterLoginPref clicked");
 
 						startActivity(new Intent(preference.getContext(),
 								LoginActivity.class));
 
 						return true;
-            }
-        });
+					}
+				});
+
+		// Configure service preference change listener
+		runInBackgroundListener = new RunInBackgroundListener();
+		runInBackgroundPref = (Preference) findPreference("runInBackground");
+		runInBackgroundPref
+				.setOnPreferenceChangeListener(runInBackgroundListener);
 
 		Log.v(TAG, "Finished onCreate");
 	}
@@ -49,5 +61,21 @@ public class SettingsActivity extends PreferenceActivity {
 		autoRotatePreference = prefs.getBoolean("rotatePref", true);
 		usernamePreference = prefs.getString("usernamePref",
 				"Nothing has been entered");
+	}
+
+	private class RunInBackgroundListener implements OnPreferenceChangeListener {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			Intent intent = new Intent(SettingsActivity.this,
+					NetworkService.class);
+
+			Boolean value = (Boolean) newValue;
+			if (value) {
+				SettingsActivity.this.startService(intent);
+			} else {
+				SettingsActivity.this.stopService(intent);
+			}
+			return true;
+		}
 	}
 }
---
+++ project/shout/src/org/whispercomm/shout/customwidgets/RunInBackgroundPreference.java
@@ -0,0 +1,20 @@
+package org.whispercomm.shout.customwidgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class RunInBackgroundPreference extends ShoutCheckBoxPreference {
+
+	public RunInBackgroundPreference(Context context) {
+		super(context);
+	}
+
+	public RunInBackgroundPreference(Context context, AttributeSet attributes) {
+		super(context, attributes);
+	}
+
+	public RunInBackgroundPreference(Context context, AttributeSet attributes,
+			int defStyle) {
+		super(context, attributes, defStyle);
+	}
+}
