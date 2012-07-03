67bfc343c4fa658a3cedb5ce55e3a7abfa51c28f
David Adrian <davadria@umich.edu>
2012-07-03 11:48:00 -0400

    Add manually serialized Shouts to test project
    Create blank test to use those Shouts in.

---
+++ project/shout/test/org/whispercomm/shout/serialization/SerializeStandardTest.java
@@ -0,0 +1,10 @@
+
+package org.whispercomm.shout.serialization;
+
+import org.junit.runner.RunWith;
+import org.whispercomm.shout.test.ShoutTestRunner;
+
+@RunWith(ShoutTestRunner.class)
+public class SerializeStandardTest {
+
+}
