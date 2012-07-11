489999f64a39704518be3d7b0dfc06598d413ccf
David Adrian <davadria@umich.edu>
2012-07-11 16:43:12 -0400

    Initial edits to improve DetailsActivity views
    Currently only has a ShoutView and a ShoutChainView, for
    proof-of-concept. The ShoutView needs to be edited into some sort of
    details view and the entire thing needs some cleaning up.

--- project/shout/src/org/whispercomm/shout/DetailsActivity.java
+++ project/shout/src/org/whispercomm/shout/DetailsActivity.java
@@ -1,29 +1,21 @@
 
 package org.whispercomm.shout;
 
+import org.whispercomm.shout.customwidgets.ShoutChainView;
+import org.whispercomm.shout.customwidgets.ShoutView;
 import org.whispercomm.shout.provider.ShoutProviderContract;
-import org.whispercomm.shout.util.ShoutMessageUtility;
 
-import android.app.ListActivity;
-import android.content.Context;
-import android.database.Cursor;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CursorAdapter;
-import android.widget.TextView;
 
 // TODO Make this page not look terrible
-public class DetailsActivity extends ListActivity {
+public class DetailsActivity extends Activity {
 
 	private static final String TAG = "DetailsActivity";
 	public static final String SHOUT_ID = "shout_id";
 	private LocalShout shout;
-
-	private Cursor cursor;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -45,8 +37,13 @@ public class DetailsActivity extends ListActivity {
 				parentHash = shout.getHash();
 				break;
 		}
-		cursor = ShoutProviderContract.getComments(getApplicationContext(), parentHash);
-		setListAdapter(new CommentsAdapter(this, cursor));
+		LocalShout parent = ShoutProviderContract.retrieveShoutByHash(getApplicationContext(),
+				parentHash);
+		ShoutView shoutView = (ShoutView) findViewById(R.id.shoutview);
+		shoutView.bindShout(shout);
+		ShoutChainView view = (ShoutChainView) findViewById(R.id.commentsview);
+		view.bindShouts(parent);
+		view.setVisibility(View.VISIBLE);
 	}
 
 	private LocalShout getShoutFromBundle(Bundle bundle) {
@@ -64,71 +61,11 @@ public class DetailsActivity extends ListActivity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		TextView username = (TextView) findViewById(R.id.origsender);
-		username.setText(shout.getSender().getUsername());
-		TextView age = (TextView) findViewById(R.id.age);
-		age.setText(ShoutMessageUtility.getDateTimeAge(shout.getTimestamp()));
-		TextView message = (TextView) findViewById(R.id.message);
-		message.setText(shout.getMessage());
-		TextView signature = (TextView) findViewById(R.id.signature);
-		signature.setText(Base64.encodeToString(shout.getSignature(),
-				Base64.DEFAULT));
-		TextView hash = (TextView) findViewById(R.id.hash);
-		hash.setText(Base64.encodeToString(shout.getHash(), Base64.DEFAULT));
-		Log.v(TAG, "Finished onResume");
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		cursor.close();
 		Log.v(TAG, "Finished onDestroy");
-	}
-
-	static class DetailsViewHolder {
-		TextView origSender;
-		TextView message;
-		TextView age;
-		byte[] hash;
-	}
-
-	private class CommentsAdapter extends CursorAdapter {
-
-		public CommentsAdapter(Context context, Cursor c) {
-			super(context, c);
-		}
-
-		@Override
-		public void bindView(View view, Context context, Cursor cursor) {
-			// Get the shout
-			LocalShout comment = ShoutProviderContract.retrieveShoutFromCursor(
-					getApplicationContext(), cursor);
-
-			// Set the view content
-			DetailsViewHolder holder = (DetailsViewHolder) view.getTag();
-			holder.hash = comment.getHash();
-			holder.age.setText(ShoutMessageUtility.getDateTimeAge(comment
-					.getTimestamp()));
-			holder.message.setText(comment.getMessage());
-			holder.origSender.setText(comment.getSender().getUsername());
-		}
-
-		@Override
-		public View newView(Context context, Cursor cursor, ViewGroup parent) {
-			// Inflate the relevant views, and return the root view for the row.
-			// These view come from comment.xml
-			// You may want to make a class similar to ShoutActivity.ViewHolder
-			// to do this.
-			LayoutInflater inflater = LayoutInflater.from(context);
-			View rowView = inflater.inflate(R.layout.comment, parent, false);
-			DetailsViewHolder holder = new DetailsViewHolder();
-			holder.origSender = (TextView) rowView
-					.findViewById(R.id.commentingUser);
-			holder.age = (TextView) rowView.findViewById(R.id.age);
-			holder.message = (TextView) rowView.findViewById(R.id.commentText);
-			rowView.setTag(holder);
-			Log.v(TAG, "View inflated");
-			return rowView;
-		}
 	}
 }
