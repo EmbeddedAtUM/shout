ceba96034f5365523d51d3d9ab71cdfb528b96a2
Gulshan Singh <gulshan@umich.edu>
2012-10-26 19:54:36 -0400

    moved the title bar to its own layout

--- project/shout/src/org/whispercomm/shout/ui/fragment/ShoutListFragment.java
+++ project/shout/src/org/whispercomm/shout/ui/fragment/ShoutListFragment.java
@@ -9,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ShoutFragment extends Fragment {
+public class ShoutListFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
