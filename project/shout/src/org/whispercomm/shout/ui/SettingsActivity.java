a58c305c36e7bbddb44e80cdc8eeecdd4e69c157
David R. Bild <drbild@umich.edu>
2012-11-01 20:19:40 -0400

    warn user when he disables the background service

--- project/shout/src/org/whispercomm/shout/ui/SettingsActivity.java
+++ project/shout/src/org/whispercomm/shout/ui/SettingsActivity.java
@@ -4,10 +4,14 @@ package org.whispercomm.shout.ui;
 import org.whispercomm.shout.R;
 import org.whispercomm.shout.network.service.NetworkService;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
@@ -16,6 +20,8 @@ import android.util.Log;
 
 public class SettingsActivity extends PreferenceActivity {
 	private static final String TAG = SettingsActivity.class.getSimpleName();
+
+	private static final int DIALOG_RUN_IN_BACKGROUND_WARNING_ID = 0;
 
 	/**
 	 * Starts the settings activity.
@@ -30,8 +36,9 @@ public class SettingsActivity extends PreferenceActivity {
 	boolean autoRotatePreference;
 	String usernamePreference;
 
+	private Intent networkServiceIntent;
 	private RunInBackgroundListener runInBackgroundListener;
-	private Preference runInBackgroundPref;
+	private CheckBoxPreference runInBackgroundPref;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -39,9 +46,11 @@ public class SettingsActivity extends PreferenceActivity {
 		setContentView(R.layout.settings);
 		addPreferencesFromResource(R.xml.preferences);
 
+		networkServiceIntent = new Intent(this, NetworkService.class);
+
 		// Configure service preference change listener
 		runInBackgroundListener = new RunInBackgroundListener();
-		runInBackgroundPref = (Preference) findPreference("runInBackground");
+		runInBackgroundPref = (CheckBoxPreference) findPreference("runInBackground");
 		runInBackgroundPref
 				.setOnPreferenceChangeListener(runInBackgroundListener);
 
@@ -63,16 +72,43 @@ public class SettingsActivity extends PreferenceActivity {
 	private class RunInBackgroundListener implements OnPreferenceChangeListener {
 		@Override
 		public boolean onPreferenceChange(Preference preference, Object newValue) {
-			Intent intent = new Intent(SettingsActivity.this,
-					NetworkService.class);
 
 			Boolean value = (Boolean) newValue;
 			if (value) {
-				SettingsActivity.this.startService(intent);
+				SettingsActivity.this.startService(networkServiceIntent);
 			} else {
-				SettingsActivity.this.stopService(intent);
+				showDialog(DIALOG_RUN_IN_BACKGROUND_WARNING_ID);
+				SettingsActivity.this.stopService(networkServiceIntent);
 			}
 			return true;
 		}
+	}
+
+	protected Dialog onCreateDialog(int id) {
+		if (id == DIALOG_RUN_IN_BACKGROUND_WARNING_ID) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this)
+					.setTitle(R.string.settings_runinbackground_warning_title)
+					.setIcon(R.drawable.icon)
+					.setMessage(R.string.settings_runinbackground_warning_message)
+					.setCancelable(false)
+					.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// Do nothing.
+						}
+					}).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							runInBackgroundPref.setChecked(true);
+							/*
+							 * onPreferenceChangeListener() is not called, so
+							 * enable service here.
+							 */
+							SettingsActivity.this.startService(networkServiceIntent);
+						}
+					});
+			return builder.create();
+		}
+		return super.onCreateDialog(id);
 	}
 }
