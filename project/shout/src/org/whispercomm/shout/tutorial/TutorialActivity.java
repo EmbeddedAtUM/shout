20c6ddcfbf3067d29867a35f6e5b167002775c79
Anthony Tesija <atesija@umich.edu>
2012-11-14 13:28:36 -0500

    Back button ends tutorial screen at any point and move to main screen

--- project/shout/src/org/whispercomm/shout/tutorial/TutorialActivity.java
+++ project/shout/src/org/whispercomm/shout/tutorial/TutorialActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -26,6 +27,15 @@ public class TutorialActivity extends Activity implements OnTouchListener {
 
 		LinearLayout layMain = (LinearLayout) findViewById(R.id.layout_main);
 		layMain.setOnTouchListener((OnTouchListener) this);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			finish();
+			return true;
+		}
+		return false;
 	}
 
 	public static void show(Context context)
