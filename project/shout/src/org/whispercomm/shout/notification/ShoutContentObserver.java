649c78f15bc69933fca40a8e4edd112d0a34b904
Gulshan Singh <gulshan@umich.edu>
2012-11-20 18:00:00 -0500

    Refactored notification sending functionality into separate class

--- project/shout/src/org/whispercomm/shout/notification/ShoutContentObserver.java
+++ project/shout/src/org/whispercomm/shout/notification/ShoutContentObserver.java
@@ -1,18 +1,12 @@
 
 package org.whispercomm.shout.notification;
 
-import org.whispercomm.shout.R;
 import org.whispercomm.shout.Shout;
 import org.whispercomm.shout.provider.ShoutProviderContract;
 import org.whispercomm.shout.provider.ShoutProviderContract.Shouts;
 import org.whispercomm.shout.ui.AbstractShoutActivity;
-import org.whispercomm.shout.ui.DetailsActivity;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
@@ -20,28 +14,13 @@ import android.os.Handler;
 
 public class ShoutContentObserver extends ContentObserver {
 
-	private static final int SHOUT_RECEIVED_NOTIFICATION_ID = 1;
-
-	private static final int icon = R.drawable.notification_icon;
-
-	private static final CharSequence contentTitle = "Shout Received";
-
 	private Context context;
-	private NotificationManager notificationManager;
+	private ShoutNotificationManager notificationManager;
 
 	public ShoutContentObserver(Handler h, Context context) {
 		super(h);
 		this.context = context;
-		notificationManager = (NotificationManager) context
-				.getSystemService(Context.NOTIFICATION_SERVICE);
 		context.getContentResolver().registerContentObserver(Shouts.CONTENT_URI, true, this);
-	}
-
-	private PendingIntent createIntent(Shout shout) {
-		Intent intent = new Intent(context, DetailsActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		intent.putExtra(DetailsActivity.SHOUT_ID, shout.getHash());
-		return PendingIntent.getActivity(context, 0, intent, 0);
 	}
 
 	// TODO Ideally, we want to call something like
@@ -66,32 +45,9 @@ public class ShoutContentObserver extends ContentObserver {
 			Shout shout = ShoutProviderContract.retrieveShoutFromCursor(context, cursor);
 			// Only notify if it is not a reshout
 			if (shout.getMessage() != null) {
-				sendNotification(shout);
+				notificationManager.sendNotification(shout);
 			}
 			cursor.close();
 		}
-	}
-
-	public void sendNotification(Shout shout) {
-		String author = shout.getSender().getUsername();
-		String message = shout.getMessage();
-		String shoutText = author + ": " + message;
-		Notification notification = constructNotification(shoutText, shoutText, createIntent(shout));
-		notificationManager.notify(SHOUT_RECEIVED_NOTIFICATION_ID, notification);
-	}
-
-	public Notification constructNotification(String tickerText, String contentText,
-			PendingIntent pIntent) {
-		// Show the notification now
-		long when = System.currentTimeMillis();
-
-		// Construct the notification
-		Notification notification = new Notification(icon, tickerText, when);
-		notification.setLatestEventInfo(context, contentTitle, contentText, pIntent);
-
-		// Remove the notification after it is clicked
-		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-
-		return notification;
 	}
 }
---
+++ project/shout/src/org/whispercomm/shout/notification/ShoutNotificationManager.java
@@ -0,0 +1,58 @@
+
+package org.whispercomm.shout.notification;
+
+import org.whispercomm.shout.R;
+import org.whispercomm.shout.Shout;
+import org.whispercomm.shout.ui.DetailsActivity;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+public class ShoutNotificationManager {
+	private NotificationManager notificationManager;
+	private Context context;
+	public static final int SHOUT_RECEIVED_NOTIFICATION_ID = 1;
+
+	private static final int icon = R.drawable.notification_icon;
+
+	private static final CharSequence contentTitle = "Shout Received";
+
+	public ShoutNotificationManager(Context context) {
+		this.context = context;
+		notificationManager = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+	}
+
+	public void sendNotification(Shout shout) {
+		String author = shout.getSender().getUsername();
+		String message = shout.getMessage();
+		String shoutText = author + ": " + message;
+		Notification notification = constructNotification(shoutText, shoutText, createIntent(shout));
+		notificationManager.notify(SHOUT_RECEIVED_NOTIFICATION_ID, notification);
+	}
+
+	private Notification constructNotification(String tickerText, String contentText,
+			PendingIntent pIntent) {
+		// Show the notification now
+		long when = System.currentTimeMillis();
+
+		// Construct the notification
+		Notification notification = new Notification(icon, tickerText, when);
+		notification.setLatestEventInfo(context, contentTitle, contentText, pIntent);
+
+		// Remove the notification after it is clicked
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		return notification;
+	}
+
+	private PendingIntent createIntent(Shout shout) {
+		Intent intent = new Intent(context, DetailsActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		intent.putExtra(DetailsActivity.SHOUT_ID, shout.getHash());
+		return PendingIntent.getActivity(context, 0, intent, 0);
+	}
+}
