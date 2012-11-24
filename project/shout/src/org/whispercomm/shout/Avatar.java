8da1ccba2493b66e8631b11a1ccea3f12945573d
David R. Bild <drbild@umich.edu>
2012-11-24 02:32:55 -0500

    add two new constructors to Avatar
    This commit adds two constructors to Avatar.
    
    The first takes a Bitmap and generates a PNG encoding for saving and
    transmission.
    
    The second takes a byte array and mime type.  If the encoded data is
    not of that mime type (even if it is a valid image type), an exception
    is thrown.  This constructor is useful when decoding Avatars from a
    ContentDescriptor for rejecting one with an incorrect mime type.

--- project/shout/src/org/whispercomm/shout/Avatar.java
+++ project/shout/src/org/whispercomm/shout/Avatar.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.whispercomm.shout.util.ImageUtils;
 
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 
 /**
@@ -19,6 +21,10 @@ import android.graphics.BitmapFactory;
  */
 public class Avatar {
 	public static final int MAX_LEN = 100 * 1024; // 100 KB
+
+	private static final int DEFAULT_COMPRESS_QUALITY = 100;
+	private static final CompressFormat DEFAULT_COMPRESS_FORMAT = CompressFormat.PNG;
+	private static final MimeType DEFAULT_MIME_TYPE = MimeType.PNG;
 
 	private final byte[] data;
 
@@ -50,6 +56,43 @@ public class Avatar {
 					"Stream does not contain a valid jpeg, gif, or png image");
 
 		mimetype = MimeType.get(options.outMimeType);
+	}
+
+	/**
+	 * Constructs an (@code Avatar} from a jpeg-, gif-, or png-encoded image.
+	 * 
+	 * @param data
+	 * @param mimetype
+	 * @throws IllegalArgumentException if {@code data} is larger than
+	 *             {@link #MAX_LEN} bytes or the specified mime type does not
+	 *             match the specified data
+	 */
+	public Avatar(byte[] data, MimeType mimetype) {
+		this(data);
+		if (!this.mimetype.equals(mimetype))
+			throw new IllegalArgumentException(String.format(
+					"Incorrect mimetype. %s was specified, but data was decoded as %s.",
+					this.mimetype, mimetype));
+	}
+
+	/**
+	 * Constructs an (@code Avatar} from the given image using the default
+	 * encoding.
+	 * 
+	 * @param data
+	 * @param mimetype
+	 * @throws IllegalArgumentException if the encoded image is larger than
+	 *             {@link #MAX_LEN} bytes
+	 */
+	public Avatar(Bitmap bitmap) {
+		this.bitmap = bitmap;
+		this.data = ImageUtils.compressBitmap(bitmap, DEFAULT_COMPRESS_FORMAT,
+				DEFAULT_COMPRESS_QUALITY);
+		this.mimetype = DEFAULT_MIME_TYPE;
+
+		if (data.length > MAX_LEN)
+			throw new IllegalArgumentException(String.format(
+					"Avatar data cannot be more than %d bytes.  Got %d.", MAX_LEN, data.length));
 	}
 
 	/**
