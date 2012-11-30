d64f5197a190f67204cbb6d5d394a34dc890bc20
David R. Bild <drbild@umich.edu>
2012-11-30 09:19:28 -0500

    add pin action bar icon

---
+++ logos/ic_menu_pin_white/install-bitmaps.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+FILENAME_BASE=ic_menu_pin_white
+RESOURCES_DIR=../../project/shout/res
+
+install_bitmap() {
+    # $1 is filename base
+    # $2 is dpi (ldpi, mdpi, etc)
+    # $3 is resources dir
+    BASE=$1
+    DPI=$2
+    RESDIR=$3
+
+    INFILE=$BASE-$DPI.png
+    OUTFILE=$RESDIR/drawable-$DPI/$BASE.png
+    
+    CMD="cp $INFILE $OUTFILE"
+    echo $CMD
+    $CMD
+}
+
+for DPI in ldpi mdpi hdpi xhdpi; do
+    install_bitmap $FILENAME_BASE $DPI $RESOURCES_DIR
+done
