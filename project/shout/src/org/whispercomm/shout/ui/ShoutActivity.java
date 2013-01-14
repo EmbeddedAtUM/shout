0e7d0e623e109901728cfbf3b0b09fd8fdd146ca
Gulshan Singh <gulshan@umich.edu>
2013-01-13 19:07:28 -0500

    Moved ShoutActivity logic into ShoutListFragment

--- project/shout/src/org/whispercomm/shout/ui/ShoutActivity.java
+++ project/shout/src/org/whispercomm/shout/ui/ShoutActivity.java
@@ -1,22 +1,10 @@
 
 package org.whispercomm.shout.ui;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.whispercomm.shout.LocalShout;
 import org.whispercomm.shout.R;
-import org.whispercomm.shout.provider.ParcelableShout;
-import org.whispercomm.shout.provider.ShoutProviderContract;
-import org.whispercomm.shout.tutorial.TutorialActivity;
 import org.whispercomm.shout.tutorial.TutorialManager;
-import org.whispercomm.shout.ui.widget.TimelineAdapter;
 
-import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
 
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
@@ -29,66 +17,15 @@ import com.actionbarsherlock.view.MenuInflater;
  */
 public class ShoutActivity extends AbstractShoutViewActivity {
 
-	private static final String BUNDLE_KEY = "parceled_shouts";
-
-	private Cursor cursor;
-
-	private Set<LocalShout> expandedShouts;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
 
-	@Override
-	protected void onDestroy() {
-		uninitialize();
-		super.onDestroy();
-	}
-
 	protected void initialize() {
 		super.initialize();
-
 		TutorialManager.showHelp(this);
-
 		setContentView(R.layout.shout_activity);
-
-		this.cursor = ShoutProviderContract
-				.getCursorOverAllShouts(getApplicationContext());
-		this.expandedShouts = new HashSet<LocalShout>();
-
-		ListView listView = (ListView) findViewById(android.R.id.list);
-		listView.setEmptyView(findViewById(android.R.id.empty));
-		listView.setAdapter(new TimelineAdapter(this, cursor, expandedShouts));
-	}
-
-	private void uninitialize() {
-		if (this.cursor != null) {
-			this.cursor.close();
-		}
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-
-		if (expandedShouts != null) {
-			ArrayList<ParcelableShout> parceled = new ArrayList<ParcelableShout>();
-			for (LocalShout shout : expandedShouts) {
-				parceled.add(new ParcelableShout(shout));
-			}
-			outState.putParcelableArrayList(BUNDLE_KEY, parceled);
-		}
-	}
-
-	@Override
-	public void onRestoreInstanceState(Bundle saveInstanceState) {
-		ArrayList<ParcelableShout> parceled = saveInstanceState.getParcelableArrayList(BUNDLE_KEY);
-		for (ParcelableShout parceledShout : parceled) {
-			expandedShouts.add(parceledShout.getShout(this));
-		}
-
-		super.onRestoreInstanceState(saveInstanceState);
 	}
 
 	@Override
@@ -96,17 +33,5 @@ public class ShoutActivity extends AbstractShoutViewActivity {
 		MenuInflater inflater = getSupportMenuInflater();
 		inflater.inflate(R.menu.menu, menu);
 		return true;
-	}
-
-	public void onClickShout(View v) {
-		MessageActivity.shout(this);
-	}
-
-	public void onClickSettings(View v) {
-		SettingsActivity.show(this);
-	}
-
-	public void onClickHelp(View v) {
-		TutorialActivity.show(this);
 	}
 }
--- project/shout/src/org/whispercomm/shout/ui/fragment/ShoutListFragment.java
+++ project/shout/src/org/whispercomm/shout/ui/fragment/ShoutListFragment.java
@@ -1,19 +1,98 @@
 
 package org.whispercomm.shout.ui.fragment;
 
-import org.whispercomm.shout.R;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.whispercomm.shout.LocalShout;
+import org.whispercomm.shout.R;
+import org.whispercomm.shout.provider.ParcelableShout;
+import org.whispercomm.shout.provider.ShoutProviderContract;
+import org.whispercomm.shout.tutorial.TutorialActivity;
+import org.whispercomm.shout.ui.MessageActivity;
+import org.whispercomm.shout.ui.SettingsActivity;
+import org.whispercomm.shout.ui.widget.TimelineAdapter;
+
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ShoutListFragment extends Fragment {
+	private Cursor cursor;
+
+	private Set<LocalShout> expandedShouts;
+
+	private static final String BUNDLE_KEY = "parceled_shouts";
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.shout_fragment, container, false);
+		View view = inflater.inflate(R.layout.shout_fragment, container, false);
+
+		this.cursor = ShoutProviderContract
+				.getCursorOverAllShouts(getActivity());
+		this.expandedShouts = new HashSet<LocalShout>();
+
+		ListView listView = (ListView) view.findViewById(android.R.id.list);
+		listView.setEmptyView(view.findViewById(android.R.id.empty));
+		listView.setAdapter(new TimelineAdapter(getActivity(), cursor, expandedShouts));
+
+		return view;
+	}
+
+	@Override
+	public void onDestroy() {
+		uninitialize();
+		super.onDestroy();
+	}
+
+	private void uninitialize() {
+		if (this.cursor != null) {
+			this.cursor.close();
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		if (expandedShouts != null) {
+			ArrayList<ParcelableShout> parceled = new ArrayList<ParcelableShout>();
+			for (LocalShout shout : expandedShouts) {
+				parceled.add(new ParcelableShout(shout));
+			}
+			outState.putParcelableArrayList(BUNDLE_KEY, parceled);
+		}
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		if (savedInstanceState != null) {
+			ArrayList<ParcelableShout> parceled = savedInstanceState
+					.getParcelableArrayList(BUNDLE_KEY);
+			for (ParcelableShout parceledShout : parceled) {
+				expandedShouts.add(parceledShout.getShout(getActivity()));
+			}
+		}
+
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	public void onClickShout(View v) {
+		MessageActivity.shout(getActivity());
+	}
+
+	public void onClickSettings(View v) {
+		SettingsActivity.show(getActivity());
+	}
+
+	public void onClickHelp(View v) {
+		TutorialActivity.show(getActivity());
 	}
 }
