a95b59d186e4fa6011fd22217e8e05e6fe8f3cb0
David R. Bild <drbild@umich.edu>
2013-01-31 20:04:06 -0500

    add vector version of launcher icon

---
+++ logos/ic_launcher/create-bitmaps.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+FILENAME_BASE=ic_launcher
+
+export_bitmap() {
+    # $1 is filename base
+    # $2 is dpi (ldpi, mdpi, etc.)
+    # $3 is resolution (36. 48, etc.)
+    BASE=$1
+    DPI=$2
+    RES=$3
+
+    INFILE=$BASE.svg
+    OUTFILE=${BASE}-${DPI}.png
+
+    inkscape --export-png=$OUTFILE --export-width=$RES --export-height=$RES $INFILE
+}
+
+IFS_OLD=$IFS
+IFS=','
+for r in ldpi,36 mdpi,48, hdpi,72, xhdpi,96; do
+    set -- $r  # read tuple as $1, $2, etc, splitting on $IFS
+    export_bitmap $FILENAME_BASE $1 $2
+done
+IFS=$OLD_IFS
