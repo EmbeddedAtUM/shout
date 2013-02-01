a97f5d3e19ea320540a0b2ada397df8915a5c77e
David R. Bild <drbild@umich.edu>
2013-01-31 19:23:40 -0500

    add compose action bar icon

---
+++ logos/ic_menu_compose_white/create-bitmaps.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+FILENAME_BASE=ic_menu_compose_white
+
+export_bitmap() {
+    # $1 is filename base
+    # $2 is dpi (ldpi, mdpi, etc.)
+    # $3 is resolution (18, 24, etc.)
+    BASE=$1
+    DPI=$2
+    RES=$3
+
+    INFILE=$BASE.svg
+    OUTFILE=${BASE}-${DPI}.png
+
+    inkscape --export-png=$OUTFILE --export-width=$RES --export-height=$RES $INFILE
+}
+
+IFS_OLD=$IFS
+IFS=','
+for r in ldpi,18 mdpi,24, hdpi,36, xhdpi,48; do
+    set -- $r  # read tuple as $1, $2, etc, splitting on $IFS
+    export_bitmap $FILENAME_BASE $1 $2
+done
+IFS=$OLD_IFS
