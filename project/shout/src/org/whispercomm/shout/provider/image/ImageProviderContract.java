3416ccc148479ea2082d5892e7c9b53c0b485b9b
Bowen Xu <bowenxu@umich.edu>
2013-06-18 16:25:44 -0400

    Add Content Provider for images.
    This is the content provider for images. Instances of using this
    provider can be found in Picasso library.

---
+++ project/shout/src/org/whispercomm/shout/provider/image/ImageProviderContract.java
@@ -0,0 +1,40 @@
+
+package org.whispercomm.shout.provider.image;
+
+import org.whispercomm.shout.Hash;
+
+import android.net.Uri;
+
+/**
+ * This class defines constants like content URIs, column names.
+ * 
+ * @author Bowen Xu
+ */
+public class ImageProviderContract {
+
+	/**
+	 * Define the authority for content provider
+	 */
+	static final String AUTHORITY = "org.whispercomm.shout.image.provider";
+
+	/**
+	 * The content:// style URI for the Shout provider
+	 */
+	static final Uri CONTENT_URI_BASE = Uri.parse("content://" + AUTHORITY);
+
+	/**
+	 * The uri for images
+	 */
+	static Uri IMAGES_URI_BASE = Uri.withAppendedPath(CONTENT_URI_BASE, "images");
+
+	/**
+	 * Constructs the content provider {@link Uri} for a shout image.
+	 * 
+	 * @param hash the hash reference of the shout image
+	 * @return the content provider {@code Uri} for referenced image
+	 */
+	public static Uri imageUri(Hash hash) {
+		return Uri.withAppendedPath(IMAGES_URI_BASE, hash.toString());
+	}
+
+}
