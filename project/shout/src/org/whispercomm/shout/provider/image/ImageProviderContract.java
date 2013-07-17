606bfde406dfa74a00c79f276f4adfaee3075084
David R. Bild <drbild@umich.edu>
2013-07-16 21:24:41 -0400

    fixup: replace inapplicable BaseColumn stuff with method to construct Uri from Hash

--- project/shout/src/org/whispercomm/shout/provider/image/ImageProviderContract.java
+++ project/shout/src/org/whispercomm/shout/provider/image/ImageProviderContract.java
@@ -1,8 +1,9 @@
 
 package org.whispercomm.shout.provider.image;
 
+import org.whispercomm.shout.Hash;
+
 import android.net.Uri;
-import android.provider.BaseColumns;
 
 /**
  * This class defines constants like content URIs, column names.
@@ -18,45 +19,25 @@ public class ImageProviderContract {
 	 * Define the authority for content provider
 	 */
 	static final String AUTHORITY = "org.whispercomm.shout.image.provider";
+
 	/**
 	 * The content:// style URI for the Shout provider
 	 */
 	static final Uri CONTENT_URI_BASE = Uri.parse("content://" + AUTHORITY);
 
-	public static class Avatars implements BaseColumns {
-		/**
-		 * Base content URI for the table of Shouts
-		 */
-		public static final Uri CONTENT_URI = Uri.withAppendedPath(
-				CONTENT_URI_BASE, "avatars");
-		/**
-		 * Column name of the primary key.
-		 */
-		public static final String _ID = BaseColumns._ID;
+	/**
+	 * The uri for images
+	 */
+	static Uri IMAGES_URI_BASE = Uri.withAppendedPath(CONTENT_URI_BASE, "images");
 
-		/**
-		 * Column name of the value.
-		 */
-		public static final String DATA = "_data";
-	}
-
-	public static class Thumbnails implements BaseColumns {
-		/**
-		 * Base content URI for the table of Shouts
-		 */
-		public static final Uri CONTENT_URI = Uri.withAppendedPath(
-				CONTENT_URI_BASE, "thumbnails");
-
-		/**
-		 * Column name of the primary key.
-		 */
-		public static final String _ID = BaseColumns._ID;
-
-		/**
-		 * Column name of the value.
-		 */
-		public static final String DATA = "_data";
-
+	/**
+	 * Constructs the content provider {@link Uri} for a shout image.
+	 * 
+	 * @param hash the hash reference of the shout image
+	 * @return the content provider {@code Uri} for referenced image
+	 */
+	public static Uri imageUri(Hash hash) {
+		return Uri.withAppendedPath(IMAGES_URI_BASE, hash.toString());
 	}
 
 }
